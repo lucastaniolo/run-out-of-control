a07b098d5f004b6c0d326021b9a39cb1b7a75c05
taniolo <l.taniolo@pipastudios.com>
2020-07-11 15:41:02 -0300

    Implement animations and camera follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private Vector3 offset;
+    
+    // public float smoothTime = 0.3f;
+    private Vector3 velocity;
+
+    private Transform tr;
+
+    private void Awake()
+    {
+        if (!target) target = FindObjectOfType<Player>().transform;
+
+        tr = transform;
+    }
+
+    private void Update()
+    {
+        var targetPosition = new Vector3(target.position.x - offset.x, tr.position.y, tr.position.z);
+        tr.position = targetPosition;
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,13 +6,15 @@ public class Player : MonoBehaviour
     [SerializeField] private Rigidbody body;
     [SerializeField] private float speed;
     [SerializeField] private float jumpForce = 8f;
-    [SerializeField] private float airJumpForce = 4f;
-
-    // [SerializeField] private float fallMultiplier = 3f;
+    [SerializeField] private Animator animator;
 
     private Transform tr;
+    
+    private static readonly int Jump = Animator.StringToHash("Jump");
+    private static readonly int Grow = Animator.StringToHash("Grow");
+    private static readonly int Shrink = Animator.StringToHash("Shrink");
 
-    private float JumpForce => body.velocity.y > 0 ? airJumpForce : jumpForce;
+    private float JumpForce => jumpForce;
 
     public event Action ObstaclePassed;
     
@@ -39,7 +41,7 @@ public class Player : MonoBehaviour
     
     private void FixedUpdate()
     { 
-        body.MovePosition(tr.position + tr.forward * (speed * Time.deltaTime));
+        body.MovePosition(tr.position + tr.right * (speed * Time.deltaTime));
 
         // if(body.velocity.y < 0)
         //     body.velocity += Vector3.up * (Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime);
@@ -55,13 +57,18 @@ public class Player : MonoBehaviour
         switch (inputButton.InputType)
         {
             case InputType.Jump:
+                animator.SetTrigger(Jump);
+                var currentVelocity = body.velocity;
+                body.velocity = new Vector3(currentVelocity.x, 0, currentVelocity.z);
                 body.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
                 break;
             
             case InputType.Grow:
+                animator.SetTrigger(Grow);
                 break;
             
             case InputType.Shrink:
+                animator.SetTrigger(Shrink);
                 break;
             
             case InputType.Shoot:
