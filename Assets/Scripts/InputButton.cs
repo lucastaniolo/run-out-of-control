d8fca772653180c771547f8ae0165fb4da9ab215
taniolo <l.taniolo@pipastudios.com>
2020-07-11 12:57:53 -0300

    More stuff

---
+++ Assets/Scripts/InputButton.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum InputType
+{
+    Jump,
+    Grow,
+    Shrink,
+    Shoot
+}
+
+public class InputButton : MonoBehaviour
+{
+    public InputType InputType { get; private set; }
+
+    public static event Action<InputButton> InputUsed;
+
+    [SerializeField] private Button button;
+    [SerializeField] private Image background;
+    
+    private void Awake()
+    {
+        button.onClick.AddListener(OnClick);
+    }
+
+    public void SetType(InputType inputType)
+    {
+        InputType = inputType;
+        UpdateView();
+    }
+
+    private void OnClick()
+    {
+        InputUsed?.Invoke(this);
+    }
+
+    private void UpdateView()
+    {
+        background.sprite = SpriteManager.Instance.GetSprite(InputType);
+    }
+}
---
+++ Assets/Scripts/SpriteManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class SpriteManager : MonoBehaviour
+{
+    public List<SpriteByInputType> SpritesByInputType;
+    
+    public static SpriteManager Instance;
+    
+    private void Awake()
+    {
+        if (Instance)
+            Destroy(this);
+
+        Instance = this;
+    }
+
+    public Sprite GetSprite(InputType inputType) => SpritesByInputType.First(s => s.InputType == inputType).Sprite;
+}
